340751067d89268672ac9582a5d2224c0123314a
Brian Lawney <brianlawney@hsph.harvard.edu>
2023-08-10 10:25:36 -0400

    including visit pk in serialized payload

--- backend/api/serializers/subject_visit.py
+++ backend/api/serializers/subject_visit.py
@@ -6,6 +6,4 @@ from api.models.visit import SubjectVisit
 class SubjectVisitSerializer(ModelSerializer):
     class Meta:
         model = SubjectVisit
-        # explicitly enumerate fields, otherwise we
-        # get the integer PK returned, which is unnecessary
-        fields = ['subject', 'visit_id', 'date_visit']
+        fields = '__all__'
