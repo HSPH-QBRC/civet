414078e75375c2f70ca2018d75fe94d444fd0bff
Brian Lawney <brianlawney@hsph.harvard.edu>
2023-08-16 12:39:48 -0400

    Major changes to migrations files for ease in incorporating new custom User model. No data to preserve yet.

--- backend/api/migrations/0002_subjectvisit.py
+++ backend/api/migrations/0002_subjectvisit.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('api', '0001_subject'),
+        ('api', '0001_initial'),
     ]
 
     operations = [
